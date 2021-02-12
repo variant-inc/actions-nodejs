2d11cb6b30d83a7837afa8cfab2a6321a8fcb4ed
Naveen Paka <npaka@usxvar-m3l6ml85.lan>
2021-02-12 11:57:55 -0500

    Adding lazy CI steps

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+echo "Start: Setting Prerequisites"
+cd "$GITHUB_WORKSPACE"
+echo "Current directory: $(pwd)"
+
+
+export BRANCH_NAME="$GITVERSION_BRANCHNAME"
+echo "Print Branch name: $BRANCH_NAME"
+
+export GITHUB_USER="$GITHUB_REPOSITORY_OWNER"
+
+echo "End: Setting Prerequisites"
+
+echo "Start: npm install and build."
+      npm install
+      npm run build --if-present
+echo "End: npm install and build."
