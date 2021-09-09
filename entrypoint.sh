12b526a850f3edda87ac72e3d90ca5e3cccc282c
Samir Alyateem <salyateem13@gmail.com>
2021-09-09 08:03:54 -0400

    collection v0 tag

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ cd "$GITHUB_WORKSPACE"
 echo "Current directory: $(pwd)"
 
 echo "Cloning into actions-collection..."
-git clone -b v1 https://github.com/variant-inc/actions-collection.git ./actions-collection
+git clone -b v0 https://github.com/variant-inc/actions-collection.git ./actions-collection
 
 export AWS_WEB_IDENTITY_TOKEN_FILE="/token"
 echo "$AWS_WEB_IDENTITY_TOKEN" >> "$AWS_WEB_IDENTITY_TOKEN_FILE"
