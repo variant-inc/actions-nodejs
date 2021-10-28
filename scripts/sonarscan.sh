dd20cd959691b0db6d72f908c7c210cc491bd6a3
John Josef <john.josef@ruralsourcing.com>
2021-10-28 14:19:01 -0400

    testing for OPT-1335
    moved sonar_project_key to an input var
    
    dumb bash mistake
    
    more dumb bash mistakes
    
    trying something else
    
    trying something else
    
    trying something else
    
    trying something else
    
    trying something else
    
    trying something else
    
    trying something else
    
    trying something else
    
    trying something else
    
    go back to using input props instead of env vars
    
    minor changes to get input project key working
    
    updated docs
    
    trying projectBaseDir again
    
    remove unused bash code
    
    changed runner image

--- scripts/sonarscan.sh
+++ scripts/sonarscan.sh
@@ -2,17 +2,19 @@
 
 set -e
 
-export OUTPUTDIR="coverage"
-mkdir -p "$OUTPUTDIR"
-
 SONAR_ORGANIZATION="$SONAR_ORG"
+
+if [ -n "$INPUT_SONAR_PROJECT_KEY" ]; then
+  echo "Changing project key to $INPUT_SONAR_PROJECT_KEY"
+  SONAR_PROJECT_KEY="$INPUT_SONAR_PROJECT_KEY"
+fi
 
 sonar_args="-Dsonar.organization=$SONAR_ORGANIZATION \
             -Dsonar.projectKey=$SONAR_PROJECT_KEY \
+            -Dsonar.projectBaseDir=$INPUT_SONAR_PROJECT_BASE_DIR \
             -Dsonar.host.url=https://sonarcloud.io \
             -Dsonar.login=$SONAR_TOKEN \
             -Dsonar.scm.disabled=true \
-            -Dsonar.javascript.lcov.reportPaths=$OUTPUTDIR/lcov.info \
             -Dsonar.scm.revision=$GITHUB_SHA"
 
 if [ "$PULL_REQUEST_KEY" = null ]; then
