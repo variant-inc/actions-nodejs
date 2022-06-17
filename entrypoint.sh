69da16b0d44aba1edf3a0761cff4bf10b2ac3e73
Tuan Nguyen <from.tuan2@gmail.com>
2022-06-17 08:49:17 -0500

    [CLOUD-1741] pointing to actions-collections branch to test

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ cd "$GITHUB_WORKSPACE"
 echo "Current directory: $(pwd)"
 
 echo "Cloning into actions-collection..."
-git clone -b v1 https://github.com/variant-inc/actions-collection.git ./actions-collection
+git clone -b f/CLOUD-1741 https://github.com/variant-inc/actions-collection.git ./actions-collection
 
 echo "---Start: Pretest script"
 chmod +x ./actions-collection/scripts/pre_test.sh
